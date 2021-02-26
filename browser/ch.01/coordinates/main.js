cb284888564e1f1017b0b6d479303899fb7adc16
김채린 <kimchaelin13@gmail.com>
2021-02-26 16:23:41 +0900

    [ADD]자바스크립트 강의

--- browser/ch.01/coordinates/main.js
+++ browser/ch.01/coordinates/main.js
@@ -2,18 +2,25 @@ const vertical = document.querySelector('.vertical');
 const horizontal = document.querySelector('.horizontal');
 const target = document.querySelector('.target');
 const tag = document.querySelector('.tag')
-
+const targetRect = target.getBoundingClientRect();
+console.log(targetRect,'찍기')
+const targetHalfWidth = targetRect.width / 2;
+const targeHalftHeight = targetRect.height / 2 ;
 document.addEventListener('mousemove',(event)=>{
     const x = event.clientX
     const y = event.clientY
     console.log(`${x} ${y}`)
 
-    vertical.style.left = `${x}px`;
-    horizontal.style.top = `${y}px`;
-    target.style.left = `${x}px`;
-    target.style.top = `${y}px`;
+    vertical.style.transform = `translateX(${x}px)`;
+    horizontal.style.transform = `translateY(${y}px)`;
+    target.style.transform = `translate(${x - targetHalfWidth}px,${y - targeHalftHeight}px)`
+    tag.style.transform = `translate(${x}px,${y}px)`
+    // vertical.style.left = `${x}px`;
+    // horizontal.style.top = `${y}px`;
+    // target.style.left = `${x}px`;
+    // target.style.top = `${y}px`;
 
-    tag.style.left = `${x}px`;
-    tag.style.top = `${y}px`;
+    // tag.style.left = `${x}px`;
+    // tag.style.top = `${y}px`;
     tag.innerHTML = `${x}px, ${y}px`
 })
